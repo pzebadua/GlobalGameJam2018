a178255d06bc9c249cfda888bc7404823b93311f
Christian Hall <christianphall@gmail.com>
2018-01-27 03:49:58 -0500

    Started working on getting individual powered objects working with code.

--- PowerHouse/Assets/Scripts/BuildingManager.cs
+++ PowerHouse/Assets/Scripts/BuildingManager.cs
@@ -4,13 +4,65 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour {
 
+    [SerializeField]
+    RoomManager[] Rooms;
+    [SerializeField]
+    //amount of rooms in a building
+    float BuildingRooms;
+    [SerializeField]
+    float CurrentPower;
+    Random Rand;
+    [SerializeField]
+    Electronics PoweredObject;
+    [SerializeField]
+    ElectricityGauge electricityGauge;
+    //[SerializeField]
+    //GameManager GameManagerScript;
+    
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Rand = new Random();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        CurrentPower = electricityGauge.Charge;
+        PowerObjects();
 	}
+    //Look at the array of rooms and calculate what objects we are going to power
+    void PowerObjects()
+    {
+        //if (GameManagerScript.Difficulty == "Easy")
+        //{
+
+        //}
+
+        //else if (GameManagerScript.Difficulty == "Medium")
+        //{
+
+        //}
+
+        //else if (GameManagerScript.Difficulty == "Hard")
+        //{
+
+        //}
+        int random = (int)Random.Range(0, BuildingRooms);
+        PoweredObject = Rooms[random].GetComponent<RoomManager>().GetComponent<Electronics>();
+        if ((PoweredObject.PowerUsage += CurrentPower) >= 5)
+        {
+            PoweredObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+
+        StartCoroutine("PowerElectronicsTimer");
+    }
+
+    IEnumerator PowerElectronicsTimer()
+    {
+        yield return new WaitForSeconds(3.0f);
+        CurrentPower -= PoweredObject.PowerUsage;
+        PoweredObject.enabled = false;
+    }
 }
