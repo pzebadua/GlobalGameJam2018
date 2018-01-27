b8033955eab2ca26f3f2ae9acf3e4b9c8e2f09d6
Christian Hall <christianphall@gmail.com>
2018-01-26 23:35:33 -0500

    Added ElectricityGauge script and setup my scene. -Christian

---
+++ PowerHouse/Assets/Scripts/ElectricityGauge.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    [SerializeField]
+    float DegenRate;
+    [SerializeField]
+    float Capacity;
+    [SerializeField]
+    //current charge value
+    float Charge;
+    [SerializeField]
+    ButtonManager ButtonScript;
+
+    void Start ()
+    {
+        //start the game at the "sweet spot" (75-80 charge)
+        Charge = 75;
+        GetComponent<Slider>().value = Charge;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ComputeCharge();
+
+        //check to see if there is no power or a power surge
+		if (Charge <= 0)
+        {
+            NoPower();
+        }
+        else if (Charge >= 100)
+        {
+            PowerSurge();
+        }
+	}
+
+    //computes the charge to be added based on incoming input
+    void ComputeCharge()
+    {
+        //Charge = accessor from ButtonManager
+        GetComponent<Slider>().value = Charge;
+    }
+
+    //power limit hit at 100
+    void PowerSurge()
+    {
+
+    }
+
+    //power limit hit at 0
+    void NoPower()
+    {
+        
+    }
+}
