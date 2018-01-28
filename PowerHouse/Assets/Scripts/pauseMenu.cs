a2f7055a76d0ce07e56648de072d29b18997d59f
Elias Vargas <Monocle.x1@gmail.com>
2018-01-27 16:26:01 -0800

    Pause Menu Prefab

---
+++ PowerHouse/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour {
+
+    //This with tell us if the game is paused or not, we set it to not be on (false) as default.
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+	// Use this for initialization
+	public void Update () {
+        //If you press Escape the game will pause, if its paused we will Resume() game. If not then we will Pause().
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+   public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("we have quit the game");
+        Application.Quit();
+    }
+}
