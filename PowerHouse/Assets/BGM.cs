793d1d273962b417169ee77d1080ba05d101e9b8
DESKTOP-OM0H8L0\Kevin Huang <Heilong1998@gmail.com>
2018-01-28 14:28:51 -0500

    Main menu hooked ith audio

---
+++ PowerHouse/Assets/BGM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        AudioManager audio = GetComponent<AudioManager>();
+        audio.Play("BGM");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
