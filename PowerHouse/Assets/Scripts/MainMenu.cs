185fa7c060fcbe03556031bd22750ffb2c259424
Christian Hall <christianphall@gmail.com>
2018-01-27 23:47:03 -0500

    Added FailState/Game Over functionality and made sure menus were still working.

--- PowerHouse/Assets/Scripts/MainMenu.cs
+++ PowerHouse/Assets/Scripts/MainMenu.cs
@@ -9,8 +9,9 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame() //Function to Run the first level
     {
-        StartCoroutine(WaitForNextScene());
+        //StartCoroutine(WaitForNextScene());
         //SceneManager.LoadScene("Credits");//Load first level.
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()//Function to quit the game.
@@ -19,10 +20,10 @@ public class MainMenu : MonoBehaviour {
         Application.Quit();//Exit the application.
     }
 
-    IEnumerator WaitForNextScene()
-    {
-        yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene(1);
+    //IEnumerator WaitForNextScene()
+    //{
+    //    yield return new WaitForSeconds(1.5f);
+    //    SceneManager.LoadScene(1);
 
-    }
+    //}
 }
